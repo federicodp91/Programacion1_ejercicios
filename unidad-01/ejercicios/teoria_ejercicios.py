68da9a5f82a2a81179922938e69c05dba22a770e
federicodepalma.n@gmail.com <federico.depalma@cencosud.com.ar>
2026-04-12 19:11:05 -0300

    Ejercicio unidad 1

---
+++ unidad-01/ejercicios/teoria_ejercicios.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+# Explicar con tus palabras que es un algoritmo en general. Y que es un algoritmo en el contexto de la programación.
+
+"""Un algoritmo es un conjunto de intrucciones diseñado para realizar una tarea específica. 
+
+En el contexto de la programación son la base para desarrollar programas:
+Debe contemplar:
+1) Exactitud: Debe indicar un orden claro de ejecución sin ambiguedades.
+2) Definición: Ejecutando el mismo algoritmo con  la misma entrada, debe producir la misma salida.
+3) Completo: Debe considerar todas las posibilidades del problema.
+4) Finito: Debe tener un número kimitado de pasos.
+5) Entendible: Las intrucciones deben ser claras y comprensibles.
+""" 
+
+# Cuantos tipos de lenguaje de programación hay. Enumerarlos y detallar brevemente cada uno.
+
+""""
+Tipos de lenguaje:
+1) Bajo Nivel: Dependen de una maquina específica. Controlan directamente el hardware. (Lenguaje ensamblador)
+2) Alto Nivel: Independientes de la máquina. Permiten crear y gestionar aplicaciones de formas más sencillas. Ej: Python.
+"""
+
+
+# Detallar las diferencias entre un lenguaje de programación interpretado y uno compilado.
+
+"""
+Compilado: Traduce todo el código fuente a lenguaje máquina en un solo proceso
+
+Interpretado: Traduce el código línea por línea cada vez que se ejecuta. Requiere del intérprete en la computadora donde se ejecuta.
+"""
+
+
+
+
